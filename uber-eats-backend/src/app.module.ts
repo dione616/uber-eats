8fa188b317ecaacb6a13327b2f25d5e762085c57
Vitalii <Vetal160199@i.ua>
2021-09-03 14:10:34 +0300

    Connected to DB

--- uber-eats-backend/src/app.module.ts
+++ uber-eats-backend/src/app.module.ts
@@ -1,10 +1,29 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 
+console.log("process.env",process.env.DB_HOST);
+
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal:true,
+      envFilePath:process.env.NODE_ENV ==='dev'?'.env.dev':'.env.test',
+      ignoreEnvFile:process.env.NODE_ENV==='prod',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: true,
+      logging:true,
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile:join(process.cwd(),'src/schema.gql')//true - will generate schema.gql from memory
     }),
--- uber-eats-backend/src/restaurants/dtos/create-restaurant.dto.ts
+++ uber-eats-backend/src/restaurants/dtos/create-restaurant.dto.ts
@@ -1,13 +1,22 @@
 import { Field, ArgsType } from '@nestjs/graphql';
+import { IsBoolean, IsString, Length } from 'class-validator';
 
 @ArgsType()
 export class CreateRestaurantDTO{
   @Field(type=>String) 
+  @IsString()
   id:string;
+
   @Field(type=>String) 
+  @IsString()
+  @Length(4,30)
   name:string;
+
   @Field(type=>Boolean) 
+  @IsBoolean()
   vegan:boolean;
-  @Field(type=>String) 
+
+  @Field(type=>String)
+  @IsString()
   address:string;
 }
