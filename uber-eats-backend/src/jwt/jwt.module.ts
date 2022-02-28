a52029cdad268a1b85383b4be89eda3e5f10a2d8
Vitalii <Vetal160199@i.ua>
2022-02-28 18:33:20 +0200

    jwt

--- uber-eats-backend/src/jwt/jwt.module.ts
+++ uber-eats-backend/src/jwt/jwt.module.ts
@@ -1,4 +1,13 @@
-import { Module } from '@nestjs/common';
+import { DynamicModule, Module } from '@nestjs/common';
+import { JwtService } from './jwt.service';
 
 @Module({})
-export class JwtModule {}
+export class JwtModule {
+  static forRoot(): DynamicModule {
+    return {
+      module: JwtModule,
+      exports: [JwtService],
+      providers: [JwtService],
+    };
+  }
+}
